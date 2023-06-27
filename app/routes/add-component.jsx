33b84fd4de8a4469cf4affdf787ddb95d29e7032
JasmanShah <jasmanshah@gmail.com>
2023-06-27 16:14:18 +0800

    Update add-component.jsx

--- app/routes/add-component.jsx
+++ app/routes/add-component.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import LocalStorageManager from '../components/LocalStorageManager';
 
 export default function AddComponent () {
   const [data, setData] = useState([]);
